b3d2be3f214b82bc84f1b9594e514d9986727ea7
ROGUE AFRICAN <iankgitonga02@gmail.com>
2020-02-26 14:24:23 +0300

    final correction

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ $("div#design").click(function() {
 });
 $("div#content").click(function() {
     $("div#content").toggle();
-    $("div#design".toggle();
+    $("div#design").toggle();
 });
 $("div#dev").click(function() {
     $("div#dev-content").toggle();
@@ -28,56 +28,56 @@ $("#col-1").hover(function() {
     $("#photo3").fadeTo("slow",0,4);
 },(funtion)() {
     $("#text1").fadeTo("slow",0);
-    $("#photo1").fadeTo("slow,1");
+    $("#photo1").fadeTo("slow",1);
 });
 $("#col-2").hover(function() {
     $("#text2").fadeTo("slow",1);
     $("#photo2").fadeTo("slow",0,4);
 },(funtion)() {
     $("#text2").fadeTo("slow",0);
-    $("#photo2").fadeTo("slow,1");
+    $("#photo2").fadeTo("slow",1);
 });
 $("#col-4").hover(function() {
     $("#text3").fadeTo("slow",1);
     $("photo3").fadeTo("slow",0,4);
 },(funtion)() {
     $("#text3").fadeTo("slow",0);
-    $("#photo3").fadeTo("slow,1");
+    $("#photo3").fadeTo("slow",1);
 });
 $("#col-4").hover(function() {
     $("#text1").fadeTo("slow",1);
     $("#photo4").fadeTo("slow",0,4);
 },(funtion)() {
     $("#text4").fadeTo("slow",0);
-    $("#photo4").fadeTo("slow,1");
+    $("#photo4").fadeTo("slow,"1);
 });
 $("#col-5").hover(function() {
     $("#text5").fadeTo("slow",1);
     $("#photo5").fadeTo("slow",0,4);
 },(funtion)() {
     $("#text5").fadeTo("slow",0);
-    $("#photo5").fadeTo("slow,1");
+    $("#photo5").fadeTo("slow",1);
 });
 $("#col-6").hover(function() {
     $("#text6").fadeTo("slow",1);
     $("#photo6").fadeTo("slow",0,4);
 },(funtion)() {
     $("#text6").fadeTo("slow",0);
-    $("#photo6").fadeTo("slow,1");
+    $("#photo6").fadeTo("slow",1);
 });
 $("#col-7").hover(function() {
     $("#text7").fadeTo("slow",1);
     $("#photo7").fadeTo("slow",0,4);
 },(funtion)() {
     $("#text7").fadeTo("slow",0);
-    $("#photo7").fadeTo("slow,1");
+    $("#photo7").fadeTo("slow",1);
 });
 $("#col-8").hover(function() {
     $("#text8").fadeTo("slow",1);
     $("#photo8").fadeTo("slow",0,4);
 },(funtion)() {
     $("#text8").fadeTo("slow",0);
-    $("#photo8").fadeTo("slow,1");
+    $("#photo8").fadeTo("slow",1);
 });
 //Business Logic
 $("form").submit(function() {
